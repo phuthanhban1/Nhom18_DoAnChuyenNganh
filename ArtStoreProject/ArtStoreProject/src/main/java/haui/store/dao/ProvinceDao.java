3b3ae506afb351cc54dd2b8335c45ba952a31c00
q979 <s2you003@gmail.com>
2024-10-28 01:46:13 +0700

    Add file dao

---
+++ ArtStoreProject/ArtStoreProject/src/main/java/haui/store/dao/ProvinceDao.java
@@ -0,0 +1,10 @@
+package haui.store.dao;
+
+import java.util.ArrayList;
+
+import haui.store.entity.Province;
+
+@SuppressWarnings("serial")
+public class ProvinceDao extends ArrayList<Province>{
+
+}
